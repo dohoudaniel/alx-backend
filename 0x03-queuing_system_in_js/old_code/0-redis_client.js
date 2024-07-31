968c390c8b90d6433310f7a382e00d6fa6435ce2
dohoudaniel <dohoudanielfavour@gmail.com>
2024-07-31 22:11:57 +0100

    Delete code

--- 0x03-queuing_system_in_js/old_code/0-redis_client.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from 'redis';
-
-const cli = createClient();
-
-cli.on('error', err => console.log(`Redis client not connected to the server: ${err}`));
-
-cli.on('connect', () => console.log('Redis client connected to the server'));
