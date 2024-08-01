d71997a2504e54628a64d87a74eb8aea19864d0c
dohoudaniel <dohoudanielfavour@gmail.com>
2024-08-01 21:52:56 +0100

    Deletion-resilient hypermedia pagination

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -39,5 +39,32 @@ class Server:
             }
         return self.__indexed_dataset
 
-    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
-            pass
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> dict:
+        """
+        Returns the range of data requested
+        """
+        if index is None:
+            # Giving index a default value
+            index = 0
+        # validating the index using assert
+        assert isinstance(index, int)
+        assert 0 <= index < len(self.indexed_dataset())
+        assert isinstance(page_size, int) and page_size > 0
+
+        data = []  # collect all indexed data
+        next_index = index + page_size
+
+        for value in range(index, next_index):
+            if self.indexed_dataset().get(value):
+                data.append(self.indexed_dataset()[value])
+            else:
+                value += 1
+                next_index += 1
+
+        return {
+            'index': index,
+            'data': data,
+            'page_size': page_size,
+            'next_index': next_index
+        }
+        # pass
