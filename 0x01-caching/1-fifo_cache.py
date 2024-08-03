b2b98b8edc830d28fb9995158ff4ba2c4e5fe375
dohoudaniel <dohoudanielfavour@gmail.com>
2024-08-03 15:56:23 +0100

    Change method of fetching first key

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -35,8 +35,9 @@ class FIFOCache(BaseCaching):
         else:
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS \
             or not key in self.cache_data.keys():
-                listOfCacheKeys = list(iter(self.cache_data.keys()))
-                firstKey = listOfCacheKeys[0]
+                # listOfCacheKeys = list(iter(self.cache_data.keys()))
+                # firstKey = listOfCacheKeys[0]
+                firstKey = next(iter(self.cache_data.keys()))
                 del self.cache_data[firstKey]
                 print(f"DISCARD: {firstKey}")
 
