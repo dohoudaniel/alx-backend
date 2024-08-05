ff0adcb45e8735278bb1816f68a86be0cfad5220
dohoudaniel <dohoudanielfavour@gmail.com>
2024-08-05 22:21:10 +0100

    Add answers to task 0

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+A simple Flask App
+"""
+
+
+# Import statements
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home_page():
+    """
+    Renders the Home page
+    """
+    return render_template('0-index.html')
+
+
+# Running the Flask app
+if __name__ == "__main__":
+    app.run(debug=True)
