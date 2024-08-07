87363a1d04bd94733befdd5d7cc6a48080af48fc
dohoudaniel <dohoudanielfavour@gmail.com>
2024-08-07 22:56:42 +0100

    Add solution to task 5

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -4,13 +4,13 @@
 
 
 # Import statements
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, g
 from flask_babel import Babel
 
 
 class Config(object):
     """
-    Translation class
+    A translation class
     """
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
@@ -24,6 +24,34 @@ app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
+# Users' data (dictionary)
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user():
+    """
+    A function that returns a user
+    """
+    login_id = request.args.get('login_as')
+    if login_id:
+        return users.get(int(login_id))
+    return None
+
+
+@app.before_request
+def before_request() -> None:
+    """
+    A function that sets the user
+    """
+    user = get_user()
+    g.user = user
+
+
 @babel.localeselector
 def get_locale():
     """
@@ -35,17 +63,15 @@ def get_locale():
         return locale
 
     return request.accept_languages.best_match(app.config['LANGUAGES'])
-
 # babel.init_app(app, locale_selector=get_locale)
 
 
 @app.route('/')
 def home_page():
     """
-    Renders the template
-    for the home page
+    Rendering the home page
     """
-    return render_template('4-index.html')
+    return render_template('5-index.html')
 
 
 if __name__ == '__main__':
